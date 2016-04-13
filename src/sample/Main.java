c339d67832600b6efcf0df3674d83523c1bd17cd
Filip Martinsson <martinsson.filip@gmail.com>
2016-04-13 15:33:13 +0200

    -

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,37 +3,39 @@ package sample;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.awt.*;
+import java.lang.reflect.Field;
+import java.util.HashMap;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-//        btn.setOnAction(new EventHandler<ActionEvent>() {
-//
-//            @Override
-//            public void handle(ActionEvent event) {
-//                System.out.println("Hello World!");
-//            }
-//        });
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
         primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+        try {
+            Class<?> macFontFinderClass = Class.forName("com.sun.t2k.MacFontFinder");
+            Field psNameToPathMap = macFontFinderClass.getDeclaredField("psNameToPathMap");
+            psNameToPathMap.setAccessible(true);
+            psNameToPathMap.set(null, new HashMap<String, String>());
+        } catch (Exception e) {
+            // ignore
+        }
+
         launch(args);
     }
 }
