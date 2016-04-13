0160ed22b27504d922c76010067fa677b320961a
Filip Martinsson <martinsson.filip@gmail.com>
2016-04-13 16:52:12 +0200

    -

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,25 +1,52 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 
+import java.util.ArrayList;
+
 public class Controller {
 
     @FXML
-    private Text xPos;
+    private AnchorPane rooot;
+
     @FXML
-    private Text yPos;
+    private TextField xPos;
     @FXML
-    private Text zPos;
+    private TextField yPos;
+    @FXML
+    private TextField zPos;
     @FXML
     private Group mapGroup;
+    @FXML
+    private TableView posTableView;
+
+    private ArrayList<Position> positionList = new ArrayList<>();
+    ObservableList<Position> listItems = FXCollections.observableArrayList ();
+
 
     @FXML
     protected void addPosition() {
-        System.out.println("Creating position");
+        System.out.println("Adding position");
+        try{
+            Position p = new Position(Integer.parseInt(xPos.getText()), Integer.parseInt(xPos.getText()), Integer.parseInt(xPos.getText()), positionList.size());
+            positionList.add(p);
+            listItems.add(p);
+            posTableView.setItems(listItems);
+        }catch (Exception e){
+            //not a string
+        }
+
     }
     @FXML
     protected void deletePosition() {
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,10 +3,14 @@ package sample;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -16,10 +20,30 @@ import java.util.HashMap;
 
 public class Main extends Application {
 
+    @FXML
+    private TableView posTableView;
+
+    private TableColumn col1 = new TableColumn("Order");
+    private TableColumn col2 = new TableColumn("X-pos");
+    private TableColumn col3 = new TableColumn("Y-pos");
+    private TableColumn col4 = new TableColumn("Z-pos");
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
         primaryStage.setTitle("Hello World!");
+
+
+        col1.setCellValueFactory(
+                new PropertyValueFactory<Position, Integer>("id"));
+        col2.setCellValueFactory(
+                new PropertyValueFactory<Position, Integer>("x"));
+        col3.setCellValueFactory(
+                new PropertyValueFactory<Position, Integer>("y"));
+        col4.setCellValueFactory(
+                new PropertyValueFactory<Position, Integer>("z"));
+        root.
+        posTableView.getColumns().addAll(col1, col2, col3, col4);
 
         primaryStage.setScene(new Scene(root, 940, 545));
         primaryStage.show();
